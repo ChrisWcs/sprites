92e99a6402d4e8799f74f2f01daa68ee5a1e7b1a
Christian <ChrisWcs@users.noreply.github.com>
2017-10-26 10:06:54 -0600

    Add store to App with middleware

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import reducer from '../reducers/reducer';
 
 const App = () => (
-    <Provider>
+    <Provider store={createStore( reducer, applyMiddleware(thunk))}>
         <div>
         </div>
     </Provider>
