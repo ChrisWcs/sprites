7184618cc85feb110cc5759c206490e0c5145647
Christian <ChrisWcs@users.noreply.github.com>
2017-10-25 23:12:57 -0600

    Added styled-components

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+
+const App = () => (
+    <Provider>
+        <div>
+        </div>
+    </Provider>
+);
+
+export default App;
