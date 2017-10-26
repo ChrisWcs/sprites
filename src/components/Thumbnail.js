f556e838b59dd228a1e3fae402aeb2ea500c822d
Christian <ChrisWcs@users.noreply.github.com>
2017-10-26 09:56:58 -0600

    Add thumbnail component

---
+++ src/components/Thumbnail.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+
+import Row from '../styledcomps/Row';
+import Column from '../styledcomps/Column';
+import Pixel from '../styledcomps/Pixel';
+
+const Thumbnail = ({ matrix }) => (
+    <Column>
+        {matrix.map( (r, i) =>{
+            return(
+                <Row key={i}>{r.map( (p, j) => <Pixel key={j} color={p} width={10} height={10}/>)}</Row>
+            );
+        })}
+    </Column>
+);
+
+Thumbnail.propTypes = {
+    matrix: PropTypes.array
+};
+
+export default Thumbnail;
