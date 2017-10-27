50b329b20affb3078db8d707268879ccf153150f
Christian <ChrisWcs@users.noreply.github.com>
2017-10-27 14:35:53 -0600

    Update Canvas and ConCanvas to be able to dispatch actions

--- src/containers/ConCanvas.js
+++ src/containers/ConCanvas.js
@@ -1,12 +1,15 @@
 import { connect } from 'react-redux';
 import Canvas from '../components/Canvas';
+import { createUpdatePixel } from '../actions/actionCreators';
 
 const mapStateToProps = (state) => ({
     matrix: state.mat
 });
 
-const mapDispatchToProps = () => ({
-    func: () => {}
+const mapDispatchToProps = (dispatch) => ({
+    func: (r, c) => () => {
+        dispatch(createUpdatePixel(r,c));
+    }
 });
 
 const ConCanvas = connect(mapStateToProps, mapDispatchToProps)(Canvas);
