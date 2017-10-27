255002e6637ddb0e2898227a67ebd9dc97251868
Christian <ChrisWcs@users.noreply.github.com>
2017-10-27 14:29:29 -0600

    Started part of the reducers

--- src/containers/ConColorPicker.js
+++ src/containers/ConColorPicker.js
@@ -1,12 +1,16 @@
 import { connect } from 'react-redux';
+import { createUpdateColor } from '../actions/actionCreators';
 import ColorPicker from '../components/ColorPicker';
 
 const mapStateToProps = (state) => ({
     color: state.color
 });
 
-const mapDispatchToProps = () => ({
-    handleChange: () => {}
+const mapDispatchToProps = (dispatch) => ({
+    handleChange: (letter) => (event) => {
+        const {value} = event.target; 
+        dispatch(createUpdateColor(letter, value)); 
+    }
 });
 
 const ConColorPicker = connect(mapStateToProps, mapDispatchToProps)(ColorPicker);
--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -1,7 +1,24 @@
 import initialState from './initialState';
+import { UPDATE_COLOR, UPDATE_PIXEL } from '../actions/actions';
+import { copySprite } from '../utils/graphicsUtil';
 
 const reducer = (state = initialState(), action) => {
     switch(action.type){
+        case UPDATE_COLOR:
+            return {
+                ...state,
+                color: {
+                    ...state.color,
+                    [action.color]: action.value
+                }
+            };
+        case UPDATE_PIXEL:
+            let newSprite = copySprite(state.mat);
+            newSprite[action.r][action.c] = `rgb(${state.color.r}, ${state.color.b},${state.color.g})`;
+            return {
+                ...state,
+                mat: newSprite
+            };
         default:
             return state;
     }
